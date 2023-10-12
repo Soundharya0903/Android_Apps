ab189e913b0dfe6553e2ce079583df00360849d8
Soundharya Gomathi S <114283701+Soundharya0903@users.noreply.github.com>
2023-10-12 12:00:57 +0530

    Add files via upload

---
+++ network_Test/network_Test/app/src/main/java/com/example/network_test/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.network_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BroadcastReceiver networkChangeReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        networkChangeReceiver = new NetworkChangeReceiver();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Register the BroadcastReceiver to listen for network changes
+        IntentFilter intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(networkChangeReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Unregister the BroadcastReceiver when the activity is paused
+        unregisterReceiver(networkChangeReceiver);
+    }
+}
